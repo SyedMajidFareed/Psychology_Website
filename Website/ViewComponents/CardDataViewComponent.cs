cc53c3c57b0b0216d43fe1ea1c80788edacdccd8
Majid Fareed <majidtracker15@gmail.com>
2022-06-14 18:31:08 +0500

    viewComponent implemented

---
+++ Website/ViewComponents/CardDataViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Website.Models;
+namespace Website.ViewComponents
+{
+    public class CardDataViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<UserTable> list = new List<UserTable>();
+            list = UserRepository.getAllUsersEF();
+            return View(list);
+        }
+    }
+}
